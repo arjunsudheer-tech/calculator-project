6bf13cda2eebd7e7cab7d0798983ca3ebffa1392
Arjun S <arjunsudheer.tech@gmail.com>
2026-07-19 11:08:16 +0530

    Added local storage to the calculator

--- script/script.js
+++ script/script.js
@@ -1,16 +1,26 @@
-let calculation = '';
+let calculation = localStorage.getItem('calculation') || '';
+
+displayResult();
 
 function updateCalculation(value) {
   calculation += value;
   displayResult();
+  localStorage.setItem('calculation',calculation);
 }
 
 function calculateResult() {
   calculation = eval(calculation);
   displayResult();
+  localStorage.setItem('calculation',calculation);
 }
 
 function displayResult() {
   document.querySelector('.js-result')
     .innerHTML = calculation;
+}
+
+function clearButton() {
+  calculation = '';
+  displayResult();
+  localStorage.setItem('calculation',calculation);
 }
